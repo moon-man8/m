42724fac9c1986de01cfc8f8f90deb6b2f9faf89
moon-man8 <pentestbatross4@gmail.com>
2025-05-12 18:37:05 +0300

    Update new6.sh

--- new6.sh
+++ new6.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "First argument: $1"
-echo "Second argument: $2"
+
+parameter_value="$1"
+
+# Construct the new string with the parameter value
+new_string="1710454080.${parameter_value}#e1rj-h9dc"
+
+# Replace the line containing the placeholder with the new string
+# sed -i "s/1710454080\..*#e1rj-h9dc/$new_string/g" 2.txt
+sed "s/1710454080\..*#e1rj-h9dc/$new_string/g" 2.txt
+
+echo "Parameter value updated successfully in the file."
